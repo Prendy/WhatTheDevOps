dea39aea7332626d4b2946c1f6d791d615399887
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-09-23 09:50:26 +0100

    nginx start please

--- webserver/provision.sh
+++ webserver/provision.sh
@@ -4,6 +4,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install git nginx nodejs build-essential npm nodejs-legacy mongodb-org -y
+sudo service nginx start
 sudo tee -a /etc/systemd/system/mongodb.service <<EOF
 [Unit]
 Description=High-performance, schema-free document-oriented database
