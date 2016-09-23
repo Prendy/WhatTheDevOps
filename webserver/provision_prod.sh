3c3c4765c2975a4111f2c49bf3f17ba74478d462
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-09-23 15:14:25 +0100

    it WILL work now

---
+++ webserver/provision_prod.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install git nginx nodejs build-essential npm nodejs-legacy mongodb-org -y
+sudo tee -a /etc/systemd/system/mongodb.service <<EOF
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo service mongod start
+#sudo adduser --disabled-password --gecos "" andrew
+#sudo adduser andrew www-data
+#git clone https://github.com/cleahy3/node-project.git .
+sudo cp ./config/default_prod /etc/nginx/sites-available/default -f
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 0755 /var/www
+sudo service nginx restart
+#sudo cp -R /root/workspace/Andrew/project/. /var/www/html
+#sudo npm install
+sudo npm install pm2 -g
+#pm2 start app.js
