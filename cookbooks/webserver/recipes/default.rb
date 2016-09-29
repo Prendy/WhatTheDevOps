c83743b9ff051dec063da675b1c073bb6d3d6dd0
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-09-29 14:53:07 +0100

    install rspec

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -22,37 +22,37 @@ execute "gem install selenium" do
   command "gem install selenium-webdriver"
 end
 
-execute "clean up dependencies" do
-    command "apt-get -f install -y"
-end
-
-execute "install some dependencies" do
-  command "sudo apt-get install libxss1 libappindicator1 libindicator7 -y"
-end
-
-execute "download chrome" do
-  command "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-end
-
-execute "install chrome" do
-  command "sudo dpkg -i google-chrome*.deb"
-end
-
-
-
-execute "instal xvfb" do
-  command "sudo apt-get install xvfb -y"
-end
-
-execute "install chromedriver" do
-  command "sudo apt-get install unzip"
-  command "wget -N http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip"
-  command "unzip chromedriver_linux64.zip"
-  command "chmod +x chromedriver"
-  command "sudo mv -f chromedriver /usr/local/share/chromedriver"
-  command "sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver"
-  command "sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver"
-end
+# execute "clean up dependencies" do
+#     command "apt-get -f install -y"
+# end
+#
+# execute "install some dependencies" do
+#   command "sudo apt-get install libxss1 libappindicator1 libindicator7 -y"
+# end
+#
+# execute "download chrome" do
+#   command "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+# end
+#
+# execute "install chrome" do
+#   command "sudo dpkg -i google-chrome*.deb"
+# end
+#
+#
+#
+# execute "instal xvfb" do
+#   command "sudo apt-get install xvfb -y"
+# end
+#
+# execute "install chromedriver" do
+#   command "sudo apt-get install unzip"
+#   command "wget -N http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip"
+#   command "unzip chromedriver_linux64.zip"
+#   command "chmod +x chromedriver"
+#   command "sudo mv -f chromedriver /usr/local/share/chromedriver"
+#   command "sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver"
+#   command "sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver"
+# end
 
 service 'nginx' do
   action [ :enable, :start ]
