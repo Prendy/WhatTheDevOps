36d53b8e0ff48ba50c4ee4d3fe903ae56b8aa9b6
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-10-04 13:48:23 +0100

    ignored

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
-docker stop andrew-app
-docker rm andrew-app
+docker stop andrew-app andrew-api
+docker rm andrew-app andrew-api
 set -e
 
 pushd my-nodejs
@@ -10,5 +10,9 @@ popd
 pushd my-node-app
 docker build --tag my-node-app:latest .
 popd
+pushd my-node-api
+docker build --tag my-node-api:latest .
+popd
+docker run --name andrew-api -p 3001:3000 -d my-node-api:latest
 docker run --name andrew-app -p 3000:3000 -d my-node-app:latest
 docker ps -a
