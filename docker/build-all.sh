7fd81b3a44e711afc203311f15e01f16c2633d2f
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-10-10 11:21:17 +0100

    docker redis shiz

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
-docker stop andrew-app andrew-api andrew-db
-docker rm andrew-app andrew-api andrew-db
+docker stop andrew-app andrew-api andrew-db my-redis-db
+docker rm andrew-app andrew-api andrew-db my-redis-db
 find my-node-api/api/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
 find my-node-app/app/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
 set -e
@@ -23,6 +23,25 @@ popd
 pushd my-mongodb
 docker build --tag my-mongodb:latest .
 popd
+pushd my-redis-data
+docker build --tag my-redis-data:latest .
+popd
+
+echo -e "\033[0;31mChecking for my-redis-data \033[0m"
+set +e
+docker ps -a | grep my-redis-data > /dev/null
+FOUND_REDIS=$?
+set -e
+
+if [[ "$FOUND_REDIS" == "0" ]]; then
+  echo -e "\033[0;32mRedis data store already exists\033[0m"
+else
+  echo -e "\033[0;34mCreate data container\033[0m"
+  docker create --name my-redis-data my-redis-data:latest
+fi
+
+echo -e "\033[0;36mRunning everything else\033[0m"
+docker run --name my-redis-db --volumes-from my-redis-data -d redis:latest
 docker run --name andrew-db -p 27017:27017 -d my-mongodb:latest
 docker run --name andrew-api -p 3001:3000 -d my-node-api:latest
 docker run --name andrew-app -p 3000:3000 -d my-node-app:latest
